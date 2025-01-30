acc2ee2a7b1d3364cb0b883329f64fed5c9f4bcf
Nelson <nelson.ade.quadri@gmail.com>
2025-01-30 01:58:31 +0100

    Pushed codes

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>
-        
+    <div className='px-34 py-25'>
+        <div>
+          <Title> Our Services </Title>
+          <h3 className='text-4xl'>We Provide Various Directions</h3>
+        </div>
+
+        <div className='px-34 flex-wrap'>
+
+        </div>
     </div>
   )
 }
